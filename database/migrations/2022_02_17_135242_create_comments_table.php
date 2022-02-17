9f85d038320d948f4dae22c5b381ffd25ead699c
Patrick <trybikpolski@gmail.com>
2022-02-17 15:00:35 +0100

    Model and migration for comments

---
+++ database/migrations/2022_02_17_135242_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return  void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('post_id')->unsigned();
+            $table->text('content');
+            $table->string('author', 128);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return  void
+     */
+    public function down()
+    {
+        Schema::table('comments', function (Blueprint $table){
+            $table->dropForeign('posts_post_id_foreign');
+            $table->drop();
+        });
+    }
+};
