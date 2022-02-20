e13744709de20cf296f0e3b9da9211fa30daa47a
Patrick <trybikpolski@gmail.com>
2022-02-20 23:06:06 +0100

    Service for Post on browser

---
+++ app/Services/PostService.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace App\Services;
+
+
+use App\Models\Post;
+use Illuminate\Contracts\View\View;
+
+class PostService
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return  View
+     */
+    public function index(): View
+    {
+        return view('posts.index', [
+            'posts' => Post::orderBy('id', 'desc')->paginate(15)
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param   Post $post
+     * @return  View
+     */
+    public function show(Post $post): View
+    {
+        return view('posts.show', [
+            'post' => $post,
+            'comments' => $post->comments()->paginate(15)
+        ]);
+    }
+}
