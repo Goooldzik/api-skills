41c738d34c7e029a8d6bc247ad847be64ddbf8e1
Patrick <trybikpolski@gmail.com>
2022-02-20 23:08:57 +0100

    Resource for Post API

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use JsonSerializable;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param   Request $request
+     * @return  array|Arrayable|JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'content' => $this->content,
+            'comments' => CommentResource::collection($this->comments),
+            'created_at' => $this->created_at->format('d-m-Y H:i'),
+            'updated_at' => $this->updated_at->format('d-m-Y H:i'),
+        ];
+    }
+}
