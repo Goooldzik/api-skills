5a5a4724dab6b384fa3fa5ed1bc44b9e37b3cf8d
Patrick <trybikpolski@gmail.com>
2022-02-22 17:07:48 +0100

    Added observers to EventServiceProvider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
+use App\Observers\CommentObserver;
+use App\Observers\PostObserver;
+use App\Observers\UserObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -27,7 +33,9 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        User::observe(UserObserver::class);
+        Post::observe(PostObserver::class);
+        Comment::observe(CommentObserver::class);
     }
 
     /**
