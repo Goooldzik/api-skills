2d8b5ab7bb78d0ae3b69258f88907a970cdd34f6
Patrick <trybikpolski@gmail.com>
2022-02-22 01:05:42 +0100

    Created service for comments on API

---
+++ app/Services/API/CommentAPIService.php
@@ -0,0 +1,88 @@
+<?php
+
+
+namespace App\Services\API;
+
+
+use App\Http\Requests\CommentRequest;
+use App\Http\Resources\CommentResource;
+use App\Models\Comment;
+use App\Models\Post;
+use Illuminate\Http\JsonResponse;
+
+class CommentAPIService
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return  JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        return response()->json([
+            'success'   =>    true,
+            'comments'  =>    CommentResource::collection(Comment::paginate(15))
+        ])->setStatusCode(200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param   CommentRequest $request
+     * @param   Post $post
+     * @return  JsonResponse
+     */
+    public function store(CommentRequest $request, Post $post): JsonResponse
+    {
+        $post->comments()->create($request->validated());
+
+        return response()->json([
+            'success'   =>    true,
+        ])->setStatusCode(201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param   Comment $comment
+     * @return  JsonResponse
+     */
+    public function show(Comment $comment): JsonResponse
+    {
+        return response()->json([
+            'success'   =>    true,
+            'comment'   =>    new CommentResource($comment)
+        ])->setStatusCode(200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param   CommentRequest $request
+     * @param   Comment $comment
+     * @return  JsonResponse
+     */
+    public function update(CommentRequest $request, Comment $comment): JsonResponse
+    {
+        $comment->update($request->validated());
+
+        return response()->json([
+            'success'   =>    true,
+        ])->setStatusCode(200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param   Comment $comment
+     * @return  JsonResponse
+     */
+    public function destroy(Comment $comment): JsonResponse
+    {
+        $comment->delete();
+
+        return response()->json([
+            'success'   =>    true,
+        ])->setStatusCode(200);
+    }
+}
