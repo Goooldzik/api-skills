d9413257d68eecd8da03c4d3826f8e53fd0c0aec
Patrick <trybikpolski@gmail.com>
2022-02-22 14:17:16 +0100

    Routing for authorization, all and one of Post and Comment

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +18,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['auth'])->group(function (){
+    Route::get('/posts', [PostController::class, 'index'])->name('index');
+    Route::get('/post/{post}', [PostController::class, 'show'])->name('posts.show');
+
+    Route::get('/comments', [CommentController::class, 'index'])->name('comments.index');
+    Route::get('/comment/{comment}', [CommentController::class, 'show'])->name('comments.show');
+
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/user/{user}', [UserController::class, 'show'])->name('users.show');
+
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+});
