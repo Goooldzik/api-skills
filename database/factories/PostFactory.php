7af541a7695b68840b06c086920a0e14742b6dcd
Patrick <trybikpolski@gmail.com>
2022-02-22 01:09:48 +0100

    Created factory for Post model

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return  array
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->text(64),
+            'content' => $this->faker->text,
+            'author' => $this->faker->firstName . $this->faker->lastName
+        ];
+    }
+}
