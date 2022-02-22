1ddfc8e2b6c29585ef49f5b32bdaa0bdd2842380
Patrick <trybikpolski@gmail.com>
2022-02-22 01:06:59 +0100

    Created resource for Comment API

---
+++ app/Http/Resources/CommentResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use JsonSerializable;
+
+class CommentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param   Request $request
+     * @return  array|Arrayable|JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'post_id' => $this->post_id,
+            'content' => $this->content,
+            'author' => $this->author,
+            'created_at' => $this->created_at->format('d-m-Y H:i'),
+            'updated_at' => $this->updated_at->format('d-m-Y H:i'),
+        ];
+    }
+}
