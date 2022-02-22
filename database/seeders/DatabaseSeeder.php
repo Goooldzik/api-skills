eed757d712984cb267e850b91209c55fefcd45c0
Patrick <trybikpolski@gmail.com>
2022-02-22 01:23:52 +0100

    Added factories do seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
+use App\Models\Post;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Post::factory(50)->create();
+        Comment::factory(50)->create();
     }
 }
